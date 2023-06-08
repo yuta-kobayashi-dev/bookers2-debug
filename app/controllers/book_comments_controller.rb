2bf53e26cabdb7478b4b257e7244e494bf7a7791
yuta-kobayashi-dev <123618181+yuta-kobayashi-dev@users.noreply.github.com>
2023-06-08 06:07:39 +0000

    いいね・コメント非同期通信追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,24 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    @book_comment = BookComment.new
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment=BookComment.find(params[:id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
 
   private
-
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 
 end
